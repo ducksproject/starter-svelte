fd70a3f47a683ff0266a9697c2a84dfabf2a9582
mathis <anonymebtc@proton.me>
2023-01-25 17:05:56 +0100

    modifié :         package-lock.json 	modifié :         package.json 	modifié :         src/App.svelte 	modifié :         src/main.js

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script>
 	export let name;
+	console.log("eeeeeeeeee");
 </script>
 
 <main>
+	<radix-connect-button />
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,40 @@
 import App from './App.svelte';
 
+
+import WalletSdk from '@radixdlt/wallet-sdk'
+
+import { configure, getMethods, requestBuilder, requestItem } from '@radixdlt/connect-button';
+
+const walletSdk = WalletSdk({ dAppId: 'dashboard', logLevel: 'DEBUG' })
+
+console.log(walletSdk);
+
+configure({
+    dAppId: 'dashboard',
+    networkId: 34,
+    logLevel: 'DEBUG',
+    onConnect: ({ setState, getWalletData }) => {
+      getWalletData({
+        oneTimeAccountsWithoutProofOfOwnership: {},
+      }).map(({ oneTimeAccounts }) => {
+        setState({ connected: true });
+        return oneTimeAccounts[0].address;
+      }).andThen(sendTx)
+    },
+    onDisconnect: ({ setState }) => {
+      setState({ connected: false });
+    },
+    onCancel() {
+      console.log('Cancel Clicked');
+    },
+    onDestroy() {
+      console.log('Button Destroyed');
+    },
+  });
+
+
+
+
 const app = new App({
 	target: document.body,
 	props: {
